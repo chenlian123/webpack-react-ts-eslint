69a20f31cb585ec23240f6effa1240ef300155d6
qianduangua <12345678>
2022-01-31 04:05:43 +0800

    添加文件

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # webpack-react-ts-eslint
+1.启动项目 
+yarn run start
+
+安装
+ yarn install / yarn
+
+
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>hello world4</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
